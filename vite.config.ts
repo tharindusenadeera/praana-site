19a89a42b4176c484278d9393f89f3a44ddccc14
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2026-01-15 22:05:53 +0000

    missing toast added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/praana-site/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [react()],
 });
